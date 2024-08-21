e603c79705b65f79a09bc0db8384adb412872cc8
Sagar Dhakal <dhakalsagar2000@gmail.com>
2024-08-21 21:42:23 +0545

    machine api created

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.auth.router import router as auth_router
 from src.auth.router import public_router as auth_public_router
 from src.vehicles.router import router as vehicle_router
+from src.machine.router import router as machine_router
 from src.utils.auth_bearer import JWTBearer
 
 app = FastAPI(
@@ -24,3 +25,4 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(auth_public_router)
 app.include_router(vehicle_router, dependencies=[Depends(JWTBearer())])
+app.include_router(machine_router, dependencies=[Depends(JWTBearer())])
